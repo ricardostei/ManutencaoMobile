0617604285b66a0c29106d1b928cdfa01a037fc4
Ricardo <ricardo.steilein@prof.sc.senac.br>
2021-10-25 14:51:49 -0300

    remove teste

--- chat.php
+++ chat.php
@@ -2,7 +2,6 @@
 
 <link rel="stylesheet" href="css/chat.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css">
-teste teste
 <div class="container-sm-2">
 <div class="row clearfix">
     <div class="col-lg-12">
