5c6a53607730211d28df4e6f0f2cf508303f07a0
Ricardo <ricardo.steilein@prof.sc.senac.br>
2021-10-25 14:49:00 -0300

    teste

--- chat.php
+++ chat.php
@@ -1,6 +1,5 @@
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-
 <link rel="stylesheet" href="css/chat.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css">
 
